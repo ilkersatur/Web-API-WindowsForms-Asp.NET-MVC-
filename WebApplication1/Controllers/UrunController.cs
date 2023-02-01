7a27f30c445a28bdf770f8d302d4c4bc87f6453a
İlker <117676317+ilkersatur@users.noreply.github.com>
2023-02-01 13:38:08 +0300

    Add files via upload

---
+++ WebApplication1/Controllers/UrunController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    public class UrunController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpClient istemci = new HttpClient();
+            string data = istemci.GetStringAsync("http://localhost:5184/api/Urun").Result;
+            var urunler = JsonConvert.DeserializeObject<List<Urun>>(data);
+            return View(urunler);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Urun urun)
+        {
+            HttpClient istemci = new HttpClient();
+
+            var resp = istemci.PostAsJsonAsync("http://localhost:5184/api/Urun", urun).Result;
+            if (resp.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("Hata",resp.Content.ReadAsStringAsync().Result);
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/Urun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Urun
+    {
+        public int UrunId { get; set; }
+        public string UrunAdi { get; set; }
+        public int KategoriId { get; set; }
+        public decimal Fiyat { get; set; }
+    }
+}
